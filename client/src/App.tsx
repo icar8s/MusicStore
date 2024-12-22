ed6c12943eb279583a244fc41c77d3c7a7fa5daf
VadimBestSorka <113136826+BrainHew@users.noreply.github.com>
2024-12-22 20:36:07 +0300

    refactor link font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Layout from "./components/Layout/Layout.tsx";
+import Layout from "./components/layout/Layout.tsx";
 import {ProtectedRoute} from "./misc/Protected.tsx";
 import {Login} from "./components/Login/Login.tsx";
 import {Products} from "./components/Products/Products.tsx";
@@ -8,43 +8,24 @@ import {useThemeStore} from "./stores/theme/useThemeStore.ts";
 import {Register} from "./components/Register/Register.tsx";
 import {Home} from "./components/Home/Home.tsx";
 import {ContactUs} from "./components/ContactUs/ContactUs.tsx";
-import {ProductDetail} from "./components/ProductDetail/ProductDetail.tsx";
+import {ProductDetails} from "./components/ProductDetails/ProductDetails.tsx";
 import {Cart} from "./components/Cart/Cart.tsx";
 
 function App() {
 
     const {selectedTheme} = useThemeStore();
-
-    const scope = import.meta.env.VITE_APP_SCOPE ?? "mstore";
-
     return <div className={`${selectedTheme}-theme container`}>
-        <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-        />
-
         <BrowserRouter>
             <Routes>
-                {
-                    scope === "mstore" ? <Route path={Layout.meta.route} element={<ProtectedRoute component={Layout}/>}>
-                            <Route path={Login.meta.route} element={<Login/>}/>
-                            <Route path={Register.meta.route} element={<Register/>}/>
-                            <Route path={Products.meta.route} element={<Products/>}/>
-                            <Route path={Home.meta.route} element={<Home/>}/>
-                            <Route path={Cart.meta.route} element={<Cart/>}/>
-                            <Route path={ContactUs.meta.route} element={<ContactUs/>}/>
-                            <Route path={ProductDetail.meta.route} element={<ProductDetail/>}/>
-                        </Route> :
-                        <Route path={Layout.meta.route} element={<ProtectedRoute component={Layout}/>}>
-                            <Route path={Login.meta.route} element={<Login/>}/>
-                            <Route path={Register.meta.route} element={<Register/>}/>
-                            <Route path={Products.meta.route} element={<Products/>}/>
-                            <Route path={Home.meta.route} element={<Home/>}/>
-                            <Route path={Cart.meta.route} element={<Cart/>}/>
-                            <Route path={ContactUs.meta.route} element={<ContactUs/>}/>
-                            <Route path={ProductDetail.meta.route} element={<ProductDetail/>}/>
-                        </Route>
-                }
+                <Route path={Layout.meta.route} element={<ProtectedRoute component={Layout}/>}>
+                    <Route path={Login.meta.route} element={<Login/>}/>
+                    <Route path={Register.meta.route} element={<Register/>}/>
+                    <Route path={Products.meta.route} element={<Products/>}/>
+                    <Route path={Home.meta.route} element={<Home/>}/>
+                    <Route path={Cart.meta.route} element={<Cart/>}/>
+                    <Route path={ContactUs.meta.route} element={<ContactUs/>}/>
+                    <Route path={ProductDetails.meta.route} element={<ProductDetails/>}/>
+                </Route>
 
                 <Route path="*" element={<>Not Found</>}/>
             </Routes>
